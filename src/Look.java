83155ff752b91d8cedf4c167cf03c962322e2f2a
hexoren21 <przemyslaw.kapelinski@o2.pl>
2018-07-07 17:14:01 +0200

    dodanie jframe

---
+++ src/Look.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Look extends JFrame implements ActionListener
+{
+    public Look()
+    {
+        setSize(418, 490);
+        setTitle("Puzzle Game");
+        setLayout(null);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+
+    }
+}
